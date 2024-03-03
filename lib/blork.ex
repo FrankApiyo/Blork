c8217bd82c44004254010c50642e97476d2a065e
apiyo <franklineapiyo@gmail.com>
2024-03-03 11:51:40 +0300

    Initial commit: first basic elixir project

---
+++ lib/blork.ex
@@ -0,0 +1,22 @@
+defmodule Blork do
+  @moduledoc """
+  Documentation for `Blork`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Blork.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def private_hello do
+    :private_world
+  end
+end
